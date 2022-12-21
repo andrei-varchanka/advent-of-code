aeabf17e21458c8633b345ad6afbd9fcb623b3be
andrei-varchanka <53002740+andrei-varchanka@users.noreply.github.com>
2022-12-21 19:42:53 +0100

    day 10 part 2

--- src/app/day-x/day-x.component.ts
+++ src/app/day-x/day-x.component.ts
@@ -8,6 +8,153 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DayXComponent implements OnInit {
 
+  data = `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`;
+
   x = 1;
   cycle = 1;
 
@@ -15,12 +162,15 @@ export class DayXComponent implements OnInit {
 
   signalStrengthsSum = 0;
 
+  canvas: Array<string[]> = Array(6).fill(0).map(() => Array(40).fill(0))
+
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
     this.httpClient.get('assets/input-data/input10.txt', { responseType: 'text' }).subscribe(data => {
       const commands = data.split('\n');
       this.runCommands(commands);
+      console.log(this.canvas.map(row => row.join('')).join('\n'));
     });
   }
 
@@ -30,6 +180,7 @@ export class DayXComponent implements OnInit {
         this.startNextCycle();
         this.startNextCycle();
         this.x += +command.split(' ')[1];
+        console.log(this.x);
       } else {
         this.startNextCycle();
       }
@@ -38,6 +189,7 @@ export class DayXComponent implements OnInit {
 
   startNextCycle() {
     this.checkSignalStrength();
+    this.draw();
     this.cycle++;
   }
 
@@ -45,6 +197,12 @@ export class DayXComponent implements OnInit {
     if (this.keyCycles.includes(this.cycle)) {
       this.signalStrengthsSum += this.cycle * this.x;
     }
+  }
+
+  draw() {
+    const i = Math.floor((this.cycle - 1) / 40);
+    const j = (this.cycle - 1) % 40;
+    this.canvas[i][j] = Math.abs(this.x - j) < 2 ? '#' : '.';
   }
 
 }
