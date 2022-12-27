1d7d87d6c64993df6159ea34a4531c934ab384f7
andrei-varchanka <53002740+andrei-varchanka@users.noreply.github.com>
2022-12-27 16:08:04 +0100

    day 19 refactored

--- src/app/components/day-xix/day-xix.component.ts
+++ src/app/components/day-xix/day-xix.component.ts
@@ -4,6 +4,18 @@ import { Component, OnInit } from '@angular/core';
 type Resource = 'ore' | 'clay' | 'obsidian' | 'geode';
 type BotRequirements = Record<Resource, number>;
 
+type State = {
+  ore: number;
+  clay: number;
+  obsidian: number;
+  geode: number;
+  oreRobots: number;
+  clayRobots: number;
+  obsidianRobots: number;
+  geodeRobots: number;
+  minutesLeft: number
+}
+
 @Component({
   selector: 'app-day-xix',
   templateUrl: './day-xix.component.html',
@@ -23,65 +35,146 @@ Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsid
   ngOnInit(): void {
     console.time("ExecutionTime");
     this.httpClient.get('assets/input-data/input19.txt', { responseType: 'text' }).subscribe(data => {
-      this.result1 = this.parseBlueprints(data).reduce((sum, blueprint, i) => sum + this.bfs(blueprint.ore, blueprint.clay, blueprint.obsidian, blueprint.geode, 24) * (i + 1), 0)
-      this.result2 = this.parseBlueprints(data).slice(0, 3).reduce((product, blueprint) => product * this.bfs(blueprint.ore, blueprint.clay, blueprint.obsidian, blueprint.geode, 32), 1);
+      const blueprints = this.parseInput(data);
+      this.result1 = blueprints.reduce((sum, blueprint, i) => sum + this.bfs(blueprint.ore, blueprint.clay, blueprint.obsidian, blueprint.geode, 24) * (i + 1), 0)
+      this.result2 = blueprints.slice(0, 3).reduce((product, blueprint) => product * this.bfs(blueprint.ore, blueprint.clay, blueprint.obsidian, blueprint.geode, 32), 1);
+      console.timeEnd("ExecutionTime");
     });
   }
 
-  parseBlueprints(data: string) {
-    return data.toLowerCase().split('\n').map(line => line.split('each ').slice(1).reduce((bots, text) => {
-      const [robot, requirements] = text.split('.')[0].split(' robot costs ')
-      bots[robot as Resource] = requirements.split(' and ').reduce((requirements, text) => {
-        const [count, resource] = text.split(' ')
-        requirements[resource as Resource] = parseInt(count)
-        return requirements
-      }, {} as BotRequirements)
-      return bots
-    }, {} as Record<Resource, BotRequirements>));
+  parseInput(data: string): Array<Record<Resource, BotRequirements>> {
+    return data.split('\n').map(blueprint => {
+      const arr = blueprint.split(' ');
+      return {
+        ore: {
+          ore: +arr[6],
+          clay: 0,
+          obsidian: 0,
+          geode: 0
+        },
+        clay: {
+          ore: +arr[12],
+          clay: 0,
+          obsidian: 0,
+          geode: 0
+        },
+        obsidian: {
+          ore: +arr[18],
+          clay: +arr[21],
+          obsidian: 0,
+          geode: 0
+        },
+        geode: {
+          ore: +arr[27],
+          clay: 0,
+          obsidian: +arr[30],
+          geode: 0
+        }
+      };
+    });
   }
 
   bfs(oreBotReq: BotRequirements, clayBotReq: BotRequirements, obsidianBotReq: BotRequirements, geodeBotReq: BotRequirements, minutesLeft: number) {
     let best = -Infinity;
     const seen = new Set();
-  
-    const stack = [[0, 0, 0, 0, 1, 0, 0, 0, minutesLeft]]
+
+    const stack: State[] = [{
+      ore: 0,
+      clay: 0,
+      obsidian: 0,
+      geode: 0,
+      oreRobots: 1,
+      clayRobots: 0,
+      obsidianRobots: 0,
+      geodeRobots: 0,
+      minutesLeft
+    }];
     while (stack.length) {
       const next = stack.pop()!;
-      let [ore, clay, obsidian, geode, oreRobots, clayRobots, obsidianRobots, geodeRobots, minutesLeft] = next;
-  
+      let { ore, clay, obsidian, geode, oreRobots, clayRobots, obsidianRobots, geodeRobots, minutesLeft } = next;
+
       best = Math.max(best, geode)
       if (minutesLeft === 0) continue;
-  
-  
+
+
       const maxOreCost = Math.max(oreBotReq.ore, clayBotReq.ore, obsidianBotReq.ore, geodeBotReq.ore);
-  
+
       oreRobots = Math.min(oreRobots, maxOreCost)
       ore = Math.min(ore, minutesLeft * maxOreCost - oreRobots * (minutesLeft - 1))
-  
+
       clayRobots = Math.min(clayRobots, obsidianBotReq.clay)
       clay = Math.min(clay, minutesLeft * obsidianBotReq.clay - clayRobots * (minutesLeft - 1))
-  
+
       geodeRobots = Math.min(geodeRobots, geodeBotReq.obsidian)
       obsidian = Math.min(obsidian, minutesLeft * geodeBotReq.obsidian - geodeRobots * (minutesLeft - 1))
-  
-  
+
+
       const key = [ore, clay, obsidian, geode, oreRobots, clayRobots, obsidianRobots, geodeRobots, minutesLeft].join(',');
       if (seen.has(key)) continue;
       seen.add(key);
-      const newMinutes = minutesLeft - 1;
-      const [newOre, newClay, newObsidian, newGeode] = [ore + oreRobots, clay + clayRobots, obsidian + obsidianRobots, geode + geodeRobots]
-      stack.push([newOre, newClay, newObsidian, newGeode, oreRobots, clayRobots, obsidianRobots, geodeRobots, newMinutes])
+      const newState = {
+        ore: ore + oreRobots,
+        clay: clay + clayRobots,
+        obsidian: obsidian + obsidianRobots,
+        geode: geode + geodeRobots,
+        oreRobots,
+        clayRobots,
+        obsidianRobots,
+        geodeRobots,
+        minutesLeft: minutesLeft - 1
+      }
+      stack.push(newState);
       if (ore >= geodeBotReq.ore && obsidian >= geodeBotReq.obsidian) {
-        stack.push([newOre - geodeBotReq.ore, newClay, newObsidian - geodeBotReq.obsidian, newGeode, oreRobots, clayRobots, obsidianRobots, geodeRobots + 1, newMinutes])
+        stack.push({
+          ore: newState.ore - geodeBotReq.ore,
+          clay: newState.clay,
+          obsidian: newState.obsidian - geodeBotReq.obsidian,
+          geode: newState.geode,
+          oreRobots,
+          clayRobots,
+          obsidianRobots,
+          geodeRobots: geodeRobots + 1,
+          minutesLeft: newState.minutesLeft
+        });
       }
       else if (ore >= obsidianBotReq.ore && clay >= obsidianBotReq.clay) {
-        stack.push([newOre - obsidianBotReq.ore, newClay - obsidianBotReq.clay, newObsidian, newGeode, oreRobots, clayRobots, obsidianRobots + 1, geodeRobots, newMinutes])
+        stack.push({
+          ore: newState.ore - obsidianBotReq.ore,
+          clay: newState.clay - obsidianBotReq.clay,
+          obsidian: newState.obsidian,
+          geode: newState.geode,
+          oreRobots,
+          clayRobots,
+          obsidianRobots: obsidianRobots + 1,
+          geodeRobots: geodeRobots,
+          minutesLeft: newState.minutesLeft
+        });
       } else {
         if (ore >= clayBotReq.ore) {
-          stack.push([newOre - clayBotReq.ore, newClay, newObsidian, newGeode, oreRobots, clayRobots + 1, obsidianRobots, geodeRobots, newMinutes])
+          stack.push({
+            ore: newState.ore - clayBotReq.ore,
+            clay: newState.clay,
+            obsidian: newState.obsidian,
+            geode: newState.geode,
+            oreRobots,
+            clayRobots: clayRobots + 1,
+            obsidianRobots: obsidianRobots,
+            geodeRobots: geodeRobots,
+            minutesLeft: newState.minutesLeft
+          });
         }
         if (ore >= oreBotReq.ore) {
-          stack.push([newOre - oreBotReq.ore, newClay, newObsidian, newGeode, oreRobots + 1, clayRobots, obsidianRobots, geodeRobots, newMinutes])
+          stack.push({
+            ore: newState.ore - oreBotReq.ore,
+            clay: newState.clay,
+            obsidian: newState.obsidian,
+            geode: newState.geode,
+            oreRobots: oreRobots + 1,
+            clayRobots: clayRobots,
+            obsidianRobots: obsidianRobots,
+            geodeRobots: geodeRobots,
+            minutesLeft: newState.minutesLeft
+          });
         }
       }
     }
