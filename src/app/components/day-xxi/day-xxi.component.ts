2522ca80229bcf6901fbd637ff7675944a5d547a
andrei-varchanka <53002740+andrei-varchanka@users.noreply.github.com>
2022-12-31 04:20:59 +0100

    day 21 part 2

--- src/app/components/day-xxi/day-xxi.component.ts
+++ src/app/components/day-xxi/day-xxi.component.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-type NumberMonkey = { key: string; number: number; };
-type OperationMonkey = { key: string; operand1: string | number; operator: string; operand2: string | number; };
+type Monkeys = {
+  [key: string]: string
+}
 
 @Component({
   selector: 'app-day-xxi',
@@ -37,69 +38,94 @@ hmdt: 32`;
 
   ngOnInit(): void {
     this.httpClient.get('assets/input-data/input21.txt', { responseType: 'text' }).subscribe(data => {
-      const [numberMonkeys, operationMonkeys] = this.parseInput(data);
-      console.log(numberMonkeys);
-      console.log(operationMonkeys);
-      this.launch(numberMonkeys, operationMonkeys);
-      this.result1 = this.knownNumbers.get('root') as number;
+      this.result1 = this.getYell(this.parseMonkeys(data));
+      this.result2 = this.getHumnValue(this.parseMonkeys(data));
     });
   }
 
-  launch(numberMonkeys: NumberMonkey[], operationMonkeys: OperationMonkey[]) {
-    while (!this.knownNumbers.has('root')) {
-      const numberMonkey = numberMonkeys.shift() as NumberMonkey;
-      this.knownNumbers.set(numberMonkey.key, numberMonkey.number);
-      operationMonkeys.forEach(operationMonkey => {
-        let canCalculate = true;
-        if (typeof operationMonkey.operand1 == 'string') {
-          if (this.knownNumbers.has(operationMonkey.operand1 as string)) {
-            operationMonkey.operand1 = this.knownNumbers.get(operationMonkey.operand1 as string) as number;
-          } else {
-            canCalculate = false;
-          }
-        }
-        if (typeof operationMonkey.operand2 == 'string') {
-          if ( this.knownNumbers.has(operationMonkey.operand2 as string)) {
-            operationMonkey.operand2 = this.knownNumbers.get(operationMonkey.operand2 as string) as number;
-          } else {
-            canCalculate = false;
-          }
-        }
-        if (canCalculate) {
-          let result;
-          if (operationMonkey.operator == '+') result = (operationMonkey.operand1 as number) + (operationMonkey.operand2 as number);
-          if (operationMonkey.operator == '-') result = (operationMonkey.operand1 as number) - (operationMonkey.operand2 as number);
-          if (operationMonkey.operator == '*') result = (operationMonkey.operand1 as number) * (operationMonkey.operand2 as number);
-          if (operationMonkey.operator == '/') result = (operationMonkey.operand1 as number) / (operationMonkey.operand2 as number);
-          numberMonkeys.push({key: operationMonkey.key, number: result as number});
-          this.knownNumbers.set(operationMonkey.key, result as number);
-          operationMonkeys.filter(monkey => monkey.key != operationMonkey.key);
-        }
-      });
+  parseMonkeys(str: string): Monkeys {
+    return str.split('\n').reduce((acc: { [key: string]: string }, cur) => {
+      const [name, operation] = cur.split(': ');
+      acc[name] = operation;
+      return acc;
+    }, {})
+  }
+
+  getYell(monkeys: Monkeys, who = 'root'): number {
+    const yell = monkeys[who];
+
+    if (/^\d+$/.test(yell)) return Number(yell);
+
+    const [left, oper, right] = yell.split(' ');
+
+    if (oper === '+') return this.getYell(monkeys, left) + this.getYell(monkeys, right);
+    if (oper === '-') return this.getYell(monkeys, left) - this.getYell(monkeys, right);
+    if (oper === '*') return this.getYell(monkeys, left) * this.getYell(monkeys, right);
+    if (oper === '/') return this.getYell(monkeys, left) / this.getYell(monkeys, right);
+
+    throw new Error('invalid yell: ' + yell);
+  }
+
+  getParent(monkeys: Monkeys, monkey: string): string {
+    const entries = Object.entries(monkeys);
+    return (entries.find(entry => entry[1].includes(monkey)) as Array<string>)[0];
+  }
+
+  getAncestry(monkeys: Monkeys, monkey: string): string[] {
+    const result = [];
+    while (monkey !== 'root') {
+      result.unshift(monkey);
+      const parent = this.getParent(monkeys, monkey);
+      monkey = parent;
     }
+    return result;
   }
 
-  parseInput(data: string): [NumberMonkey[], OperationMonkey[]] {
-    const numberMonkeys: NumberMonkey[] = [];
-    const operationMonkeys: OperationMonkey[] = [];
-    data.split('\n').forEach(monkey => {
-      let [key, value] = monkey.split(': ');
-      if (Number.isNaN(+value)) {
-        const valueArr = value.split(' ');
-        operationMonkeys.push({
-          key,
-          operand1: valueArr[0],
-          operator: valueArr[1],
-          operand2: valueArr[2]
-        });
+  getHumnValue(monkeys: Monkeys): number {
+    const ancestry = this.getAncestry(monkeys, 'humn');
+
+    const getOther = (branch: string, name: string) =>
+      monkeys[branch].split(' ').find(s => /[a-z]/.test(s) && s !== name);
+
+    const humnBranch = ancestry[0];
+    const otherBranch = getOther('root', humnBranch);
+
+    let currentNum = this.getYell(monkeys, otherBranch);
+
+    for (let i = 0; i < ancestry.length - 1; i++) {
+      const cur = ancestry[i];
+      const next = ancestry[i + 1];
+      const other = getOther(cur, next);
+
+      const otherNum = this.getYell(monkeys, other);
+
+      const [left, oper, _right] = monkeys[cur].split(' ');
+
+      if (left === next) {
+        // next ? otherNum = currentNum
+        if (oper === '+') {
+          currentNum = currentNum - otherNum;
+        } else if (oper === '-') {
+          currentNum = currentNum + otherNum;
+        } else if (oper === '*') {
+          currentNum = currentNum / otherNum;
+        } else if (oper === '/') {
+          currentNum = currentNum * otherNum;
+        }
       } else {
-        numberMonkeys.push({
-          key,
-          number: +value
-        });
+        // otherNum ? next = currentNum
+        if (oper === '+') {
+          currentNum = currentNum - otherNum;
+        } else if (oper === '-') {
+          currentNum = otherNum - currentNum;
+        } else if (oper === '*') {
+          currentNum = currentNum / otherNum;
+        } else if (oper === '/') {
+          currentNum = otherNum / currentNum;
+        }
       }
-    });
-    return [numberMonkeys, operationMonkeys];
+    }
+    return currentNum;
   }
 
 }
