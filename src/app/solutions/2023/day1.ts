7c95bca606b7b90bbbde0bab51005695c9f44e5e
Andrei Varchanka <andrei.varchanka@ventionteams.com>
2023-12-20 01:14:25 +0100

    day1 part2

--- src/app/solutions/2023/day1.ts
+++ src/app/solutions/2023/day1.ts
@@ -2,26 +2,6 @@ import {Solver } from "src/app/models/solver.model";
 
 export default class Day01 extends Solver {
 
-  data = `two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen`;
-
-  numbers: Map<string, number> = new Map([
-    ['one', 1],
-    ['two', 2],
-    ['three', 3],
-    ['four', 4],
-    ['five', 5],
-    ['six', 6],
-    ['seven', 7],
-    ['eight', 8],
-    ['nine', 9],
-  ]); 
-
   public override part1(rawInput: string) {
     let sum = 0;
     const arr = rawInput.trim().split("\n");
@@ -34,12 +14,28 @@ zoneight234
 
   public override part2(rawInput: string) {
     let sum = 0;
+    const NUMBERS = new Map([
+      ['one', 1],
+      ['two', 2],
+      ['three', 3],
+      ['four', 4],
+      ['five', 5],
+      ['six', 6],
+      ['seven', 7],
+      ['eight', 8],
+      ['nine', 9],
+    ]); 
     const arr = rawInput.trim().split("\n");
     arr.forEach(line => {
-      // numbers.forEach(number => line.replaceAll)
+      NUMBERS.forEach((value, key) => {
+        let numberArr = key.split('');
+        numberArr.splice(1, 0, value.toString());
+        const combinedNumber = numberArr.join();
+        line = line.replaceAll(key, combinedNumber);
+      });
       const numbers = line.split('').filter(char => !isNaN(parseInt(char)));
       sum += +(numbers.length > 1 ? numbers[0] + numbers[numbers.length - 1] : numbers[0] + numbers[0]);
     });
-    return 0;
+    return sum;
   }
 }
